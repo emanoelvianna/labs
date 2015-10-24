31ef11970c1f7964d243f4caa096e87bbaf93178
@emanoelvianna <emanoel.vianna@outlook.com>
2015-10-24 00:05:30 -0200

    Update BinSearch.asm

--- Assembly mips/BinSearch.asm
+++ Assembly mips/BinSearch.asm
@@ -1,6 +1,6 @@
-# Trabalho 2, pesquisa bin·ria.
+# Trabalho 2, pesquisa bin√°ria.
 #
-# Para a realizaÁ„o do trabalho utilizamos uma pilha
+# Para a realiza√ß√£o do trabalho utilizamos uma pilha
 #
 
 
@@ -8,41 +8,38 @@
 	vet : .word -5 -1 5 9 12 15 21 29 31 58 250 325 
 	prim : .word 0
 	ult : .word 11
-	meio : .word 0 # * ira variar segundo tam vetor
 	valor : .word 5 # * retorno deve ser 2
+	
+	meio : .word 0 # * ira variar segundo tam vetor
+	
 	
 .text 
 .globl main
 
 main:
-	la $t0, vet 
+	la $a0, vet 
 	
-	la $t1, prim
-	lw $t1, 0($t1)	# ref comeÁo do vetor
+	la $a1, prim
+	lw $a1, 0($a1)	# ref come√ßo do vetor
 	
-	la $t2, ult 
-	lw $t2, 0($t2) # ref final do vetor
+	la $a2, ult 
+	lw $a2, 0($a2) # ref final do vetor
 	
-	la $t3, meio
-	lw $t3, 0($t3) # meio ( auxilio para o mÈtodo )
+	la $a3, valor 
+	lw $a3, 0($a3) # valor a ser buscado
 	
-	la $t4, valor 
-	lw $t4, 0($t4) # valor a ser buscado
+	addiuj $sp, $sp, -4 # preparando para empilhar o $ra
+	sw $ra, 0($sp) # empilha o $ra
 	
-	addiu $sp, $sp, -4 # empilha o $ra
-	sw $ra, 0($sp)
 	addiu $sp, $sp, -16 # empilha o restante dos parametros
-	sw $t0, 0($sp) # passa para a pilha o A
-	sw $t1, 4($sp) # passa para a pilha o Pri
-	sw $t2, 8($sp) # passa para a pilha o Ult
-	sw $t3, 12($sp) # passa para a pilha o Tam
+	sw $a0, 0($sp) # passa para a pilha o A
+	sw $a1, 4($sp) # passa para a pilha o Pri
+	sw $a2, 8($sp) # passa para a pilha o Ult
+	sw $a3, 12($sp) # passa para a pilha o Valor a ser buscado
 	
 	jal BinSearch
 	
 	# tratando o valor do retorno da funcao
-	lw      $ra, 0($sp)      
-        lw      $t0, 4($sp)       
-        addiu   $sp, $sp, 12  
 	
 	
 	jal fim
@@ -50,12 +47,18 @@ main:
 BinSearch:
 
 	# desempilha e recursao?!
+	lw $t0, 0($sp) # desempilha o valor a ser buscado
+	lw $t1, 0($sp) # desempilha valor Ult
+	lw $t2, 0($sp) # desempilha valor Pri
+	lw $t3, 0($sp) # desempilha ref vetor A
+	
+	
 		
 	add $a1, $t1, $zero # $a1 <- ref primeiro 
 	add $a2, $t2, $zero # $a2 <- ref ultimo
-	# if (Prim > Ult) return -1; // Valor n„o existe
+	# if (Prim > Ult) return -1; // Valor n√£o existe
 	slt $s0, $a2, $a1
-	beq $s0, $0, erro # Se verdade continua, se n„o erro	
+	beq $s0, $0, erro # Se verdade continua, se n√£o erro	
 	
 	add $s1, $a1,$s2  # $s1 <- prim + ult
 	li $s2, 2 # auxiliar na divisao
