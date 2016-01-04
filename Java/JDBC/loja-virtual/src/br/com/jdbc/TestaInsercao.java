a17c0e86e0b3644701593932c1aa221c5f70a5cb
emanoelvianna <emanoel.vianna@outlook.com>
2016-01-04 21:55:12 -0200

    Projeto tranalhando com a classe PreparedStatement

--- Java/JDBC/loja-virtual/src/br/com/jdbc/TestaInsercao.java
+++ Java/JDBC/loja-virtual/src/br/com/jdbc/TestaInsercao.java
@@ -1,6 +1,7 @@
 package br.com.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,18 +9,26 @@ import java.sql.Statement;
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
+
+		String nome = "Notebook";
+		String descricao = "Notebook i5";
+
 		Connection connection = Database.getConnection();
-		Statement statement = connection.createStatement();
 		
-		boolean resultado = statement.execute("insert into Produto (nome, descricao) values ('Notebook', 'Notebook i5')", statement.RETURN_GENERATED_KEYS);
+		String sql = "insert into Produto (nome, descricao) values(?, ?)";
+		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		statement.setString(1, nome);
+		statement.setString(2, descricao);
+
+		boolean resultado = statement.execute();
 		System.out.println(resultado);
 		// devolve a lista de ids
 		ResultSet resultSet = statement.getGeneratedKeys();
-		while(resultSet.next()) {
+		while (resultSet.next()) {
 			String id = resultSet.getString("id");
 			System.out.println(id);
 		}
-		
+		resultSet.close();
 		statement.close();
 		connection.close();
 	}
