d21f5a822a3d222b46d5161469a13fc0d13dedbc
emanoelvianna <emanoel.vianna@outlook.com>
2016-01-04 22:54:10 -0200

    tratando exceções e commit no JDBC

--- Java/JDBC/loja-virtual/src/br/com/jdbc/TestaInsercao.java
+++ Java/JDBC/loja-virtual/src/br/com/jdbc/TestaInsercao.java
@@ -9,14 +9,31 @@ import java.sql.Statement;
 public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
+		// try irá fechar automaticamente a connection
+		try (Connection connection = Database.getConnection()) {
+			connection.setAutoCommit(false);
+			String sql = "insert into Produto (nome, descricao) values(?, ?)";
+			// o mesmo valie para PreparedStatement, irá ser rodado e fechado sozinho
+			try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
-		String nome = "Notebook";
-		String descricao = "Notebook i5";
+				adiciona("Notebook", "Notebook i5", statement);
+				adiciona("TV LCD", "32 polegadas", statement);
+				adiciona("Blueray", "Full HDMI", statement);
+				connection.commit();
 
-		Connection connection = Database.getConnection();
-		
-		String sql = "insert into Produto (nome, descricao) values(?, ?)";
-		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				connection.rollback();
+				System.out.println("rollback");
+			}
+		}
+	}
+
+	private static void adiciona(String nome, String descricao, PreparedStatement statement) throws SQLException {
+		if (nome.equals("Blueray")) {
+			throw new IllegalArgumentException("Problema ocorrido");
+		}
+
 		statement.setString(1, nome);
 		statement.setString(2, descricao);
 
@@ -29,7 +46,5 @@ public class TestaInsercao {
 			System.out.println(id);
 		}
 		resultSet.close();
-		statement.close();
-		connection.close();
 	}
 }
