04f6a302aec506e8f0eb0155915f0b46ecb40b63
emanoelvianna <emanoel.vianna@outlook.com>
2016-01-16 16:35:04 -0200

    cadastro de amigos - adicionado classe de conexao

--- Java/JDBC/cadastro-de-amigos/src/br/com/connection/ConnectionPool.java
+++ Java/JDBC/cadastro-de-amigos/src/br/com/connection/ConnectionPool.java
@@ -2,63 +2,27 @@ package br.com.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionPool {
+	
+	private static final String STR_DRIVER        = "org.postgresql.Driver";
+	private static final String STR_CONNECTION    = "jdbc:postgresql://localhost:5432/amigos";
+	private static final String STR_DATABASE_USER = "postgres";
+    private static final String STR_DATABASE_PWD  = "admin"; 
+	
 
-	private static final String STR_DRIVER = "org.postgresql.Driver";
-	private static final String STR_CONNECTION = "jdbc:postgresql://localhost:5432/cadastro_de_amigos";
-	private static final String STR_DATABASE_USER = "usuario";
-	private static final String STR_DATABASE_PWD = "senha";
-
-	private static ConnectionPool instance = new ConnectionPool();
-
-	static {
+	public static Connection getConnection() {
+		Connection connection = null;
 		try {
-			Class.forName(STR_DRIVER);
-		} catch (Exception e) {
-			System.out.println("Erro ao carregar driver JDBC: " + STR_DRIVER);
+			connection = DriverManager.getConnection(STR_CONNECTION, STR_DATABASE_USER, STR_DATABASE_PWD);
+			System.out.println("conexão com sucesso!");
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
+		
+		return connection;
 
-	public ConnectionPool() {};
-
-	public static ConnectionPool getInstance() {
-		return instance;
-	}
-
-	public static Connection getConnection() throws Exception {
-		return getConnectionManual();
-	}
-
-	private static Connection getConnectionManual() throws Exception {
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(STR_CONNECTION, STR_DATABASE_USER, STR_DATABASE_PWD);
-		} catch (Exception e) {
-			throw new Exception("Erro ao obter conexão via DriverManager: " + STR_CONNECTION);
-		}
-		return conn;
-	}
-
-	public void close(Connection conn, PreparedStatement pstmt, ResultSet rs) throws SQLException {
-		// Fecha o resultset aberto
-		if (rs != null) {
-			rs.close();
-		}
-		// Fecha o preparedstatement aberto
-		if (pstmt != null) {
-			pstmt.close();
-		}
-		// Fecha a conexão aberta
-		if (conn != null) {
-			if (!conn.isClosed()) {
-				conn.close();
-			}
-		}
 	}
 
 }
---
+++ Java/JDBC/cadastro-de-amigos/src/br/com/test/ConnectionPoolTest.java
@@ -0,0 +1,11 @@
+package br.com.test;
+
+import br.com.connection.ConnectionPool;
+
+public class ConnectionPoolTest {
+
+	public static void main(String[] args) {
+		ConnectionPool.getConnection();
+	}
+
+}
