632507efb0cc1de2591f79f8c34e4d87699b9ecc
emanoelvianna <emanoel.vianna@outlook.com>
2016-01-16 16:45:52 -0200

    cadastro de amigos, teste simples de conexao

--- Java/JDBC/cadastro-de-amigos/src/br/com/connection/ConnectionPool.java
+++ Java/JDBC/cadastro-de-amigos/src/br/com/connection/ConnectionPool.java
@@ -5,12 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionPool {
-	
-	private static final String STR_DRIVER        = "org.postgresql.Driver";
-	private static final String STR_CONNECTION    = "jdbc:postgresql://localhost:5432/amigos";
+
+	private static final String STR_DRIVER = "org.postgresql.Driver";
+	private static final String STR_CONNECTION = "jdbc:postgresql://localhost:5432/amigos";
 	private static final String STR_DATABASE_USER = "postgres";
-    private static final String STR_DATABASE_PWD  = "admin"; 
-	
+	private static final String STR_DATABASE_PWD = "admin";
 
 	public static Connection getConnection() {
 		Connection connection = null;
@@ -20,9 +19,6 @@ public class ConnectionPool {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 		return connection;
-
 	}
-
 }
--- Java/JDBC/cadastro-de-amigos/src/br/com/principal/Principal.java
+++ Java/JDBC/cadastro-de-amigos/src/br/com/principal/Principal.java
@@ -1,12 +1,27 @@
 package br.com.principal;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import br.com.connection.ConnectionPool;
 
 public class Principal {
 	public static void main(String[] args) throws Exception {
-		Connection connection =  ConnectionPool.getConnection();
+		Connection connection = ConnectionPool.getConnection();
+
+		Statement statement = connection.createStatement();
+		boolean resultado = statement.execute("select * from amigos");
+		ResultSet resultSet = statement.getResultSet();
 		
+		while(resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String nome = resultSet.getString("nome");
+			String telefone = resultSet.getString("telefone");
+			System.out.println(id);
+			System.out.println(nome);
+			System.out.println(telefone);
+		}
 	}
+
 }
