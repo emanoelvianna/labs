121f6b0648c7a86a6db6a1d6fd55f38b284774f2
emanoelvianna <emanoel.vianna@outlook.com>
2016-02-14 14:16:07 -0200

    Testes de unidade jasmine

--- javascript/testes-unitarios-jasmine/jasmine-standalone-2.4.1/spec/ConsultaSpec.js
+++ javascript/testes-unitarios-jasmine/jasmine-standalone-2.4.1/spec/ConsultaSpec.js
@@ -1,37 +1,31 @@
 describe("Consulta", function() {
 
+  var guilherme;
+  var pedro;
+  beforeEach(function() {
+    guilherme = new Paciente(guilherme, 32, 1.60, 70);
+    pedro = new Paciente(pedro, 22, 1.90, 80);
+  });
+
   it("não deve cobrar nada se for um retorno", function() {
-    var paciente1 = new Paciente("Pedro", 22, 1.78, 100);
-    var consulta = new Consulta(paciente1, [], false, true);
+    var consulta = new Consulta(guilherme, [], false, true);
 
     expect(consulta.preco()).toEqual(0);
   });
 
   it("deve cobrar 25 reais para procedimento comum", function() {
-    var paciente1 = new Paciente("Teste", 50, 1.68, 80);
-    var consulta1 = new Consulta(paciente1, ["coleta de sangue"], false, false);
-
+    var consulta1 = new Consulta(guilherme, ["coleta de sangue"], false, false);
     expect(consulta1.preco()).toEqual(25);
-
-    var paciente2 = new Paciente("Teste", 50, 1.68, 80);
-    var consulta2 = new Consulta(paciente2, ["coleta de sangue", "procedimento2"], false, false);
-
+    var consulta2 = new Consulta(pedro, ["coleta de sangue", "procedimento2"], false, false);
     expect(consulta2.preco()).toEqual(50);
-
   });
 
   it("consulta particular deve dobra o valor da consulta.", function() {
-    var paciente1 = new Paciente("Teste", 50, 1.68, 80);
-    var consulta1 = new Consulta(paciente1, ["coleta de sangue"], true, false);
-
+    var consulta1 = new Consulta(guilherme, ["coleta de sangue"], true, false);
     expect(consulta1.preco()).toEqual(50);
-
-    var paciente2 = new Paciente("Teste", 50, 1.68, 80);
-    var consulta2 = new Consulta(paciente2, ["coleta de sangue", "raio-x"], true, false);
+    var consulta2 = new Consulta(pedro, ["coleta de sangue", "raio-x"], true, false);
 
     expect(consulta2.preco()).toEqual(160);
 
   });
-
-
 });
