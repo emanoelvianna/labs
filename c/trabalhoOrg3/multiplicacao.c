9dc7a2f6f1e076bd5b23fe8a874e7a1ecade92d3
emanoelvianna <emanoel.vianna@outlook.com>
2016-09-24 01:38:41 -0300

    Código inicial para o trabalho de org. arquitetura III

--- c/trabalhoOrg3/multiplicacao.c
+++ c/trabalhoOrg3/multiplicacao.c
@@ -1,41 +1,59 @@
-/** Multiplicação de matrizes  **/
-// Obrigatoriamente: o numero de colunas de A deve ser igual a numero de linhas de B
+/**
+
+Multiplicação de matrizes
+Obrigatoriamente: o numero de colunas de A deve ser igual a numero de linhas de B
+
+**/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void multiplicaMatrizes(int M1[4][4],int L1, int C1, int M2[4][4], int L2, int C2, int *M3[4][4]) {
-	if(C1 != L2) {
-		printf("Soh pode haver multiplicacao se C1 = L2");
-		return;
-	}
-	for(int i = 0; i < L1; i++) {
-		for(int j = 0; j < C2; j++) {
-			M3[i][j] = 0;
-			for(int k = 0; k < C1; k++) {
-				M3[i][j] += M1[i][k] * M2[k][j];
-			}
-		}
-	}
+/** decleracao do tamanho da linha e coluna da matriz **/
+#define linha 500
+#define coluna 500
+
+int randomInteger (float low, float high)
+{
+    int k;
+    double d;
+    d = (double) rand () / ((double) RAND_MAX + 1);
+    k = d * (high - low + 1);
+    return low + k;
 }
 
 int main() {
-  int M1[4][4] = {{1,   2,    3,    4},
-                  {5,   6,    7,    8},
-                  {9,   10,   11,   12},
-                  {13,  14,   15,   16}};
 
-  int M2[4][4] = {{1,   2,    3,    4},
-                  {5,   6,    7,    8},
-                  {9,   10,   11,   12},
-                  {13,  14,   15,   16}};
+  int M1[linha][coluna];
+	int M2[linha][coluna];
+  int matrizResultante[linha][coluna];
 
-  int M3[4][4] = {{0,  0,   0,   0},
-                  {0,  0,   0,   0},
-                  {0,  0,   0,   0},
-                  {0,  0,   0,   0}};;
+	/** preenchendo a matriz com valores randomicos **/
 
-  multiplicaMatrizes(M1, 4, 4, M2, 4, 4, M3);
+	for(int i = 0; i < linha; i++) {
+		for(int j = 0; j < coluna; j++) {
+			M1[i][j] = randomInteger(100, 500);
+			M2[i][j] = randomInteger(100, 500);
+		}
+	}
+
+	/** multiplicando a matriz **/
+	for(int i = 0; i < linha; i++) {
+		for(int j = 0; j < coluna; j++) {
+			matrizResultante[i][j] = 0;
+			for(int k = 0; k < coluna; k++) {
+				matrizResultante[i][j] += M1[i][k] * M2[k][j];
+			}
+		}
+	}
+
+	/** escrevendo a matriz resultante **/
+	for(int i = 0; i < linha; i++) {
+		for(int j = 0; j < coluna; j++) {
+			printf("%d", matrizResultante[i][j]);
+			printf(" ");
+		}
+		printf("\n");
+	}
 
   return 0;
 }
