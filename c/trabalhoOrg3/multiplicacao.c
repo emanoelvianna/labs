26e8f5f8a503dd3886180cb1cbf6bb842e1deab4
emanoelvianna <emanoel.vianna@outlook.com>
2016-09-24 02:03:01 -0300

    adicionando observações

--- c/trabalhoOrg3/multiplicacao.c
+++ c/trabalhoOrg3/multiplicacao.c
@@ -3,6 +3,15 @@
 Multiplicação de matrizes
 Obrigatoriamente: o numero de colunas de A deve ser igual a numero de linhas de B
 
+Observações:
+
+Quem sabe utilizar um long double inicialmente para os valores da matriz ( para isso precisa modificar a função random ).
+Dara uma pesquisada e ver se é possivel guardar os valores quem sabe em um segundo momento em um short int.
+Também avaliar o que se pode melhorar nos loops e questões de desempenho e hierarquia de memoria.
+	* Para saber se as alteraçẽos realmente surtiram efeito precisamos utilizar os softwares!
+
+Ainda precisa se avaliar se utilizamso uma função randomica para gerar os valores
+
 **/
 
 #include <stdio.h>
@@ -46,14 +55,14 @@ int main() {
 		}
 	}
 
-	/** escrevendo a matriz resultante **/
-	for(int i = 0; i < linha; i++) {
-		for(int j = 0; j < coluna; j++) {
-			printf("%d", matrizResultante[i][j]);
-			printf(" ");
-		}
-		printf("\n");
-	}
+	// /** escrevendo a matriz resultante **/
+	// for(int i = 0; i < linha; i++) {
+	// 	for(int j = 0; j < coluna; j++) {
+	// 		printf("%d", matrizResultante[i][j]);
+	// 		printf(" ");
+	// 	}
+	// 	printf("\n");
+	// }
 
   return 0;
 }
